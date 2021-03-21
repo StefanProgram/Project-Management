91a8a9e830d2cf332415cd5eacc0ed246553e118
Mihai Bobric <mihai.bobric@outlook.com>
2021-03-21 10:33:10 +0200

    21

--- projectmanagement/src/main/java/com/sda/projectmanagement/controllers/ProjectController.java
+++ projectmanagement/src/main/java/com/sda/projectmanagement/controllers/ProjectController.java
@@ -35,21 +35,22 @@ public class ProjectController {
         projectService.addProject(newProject);
         return "redirect:/getProjects";
     }
+
     @GetMapping(path = "edit-project/{id}")
-    public String editProjectPage(Model model, @PathVariable("id") Integer id){
+    public String editProjectPage(Model model, @PathVariable("id") Integer id) {
         ProjectsEntity projectsEntity = projectService.getProject(id);
         model.addAttribute("projectToBeEdited", projectsEntity);
         return "edit-project";
     }
 
     @PostMapping(path = "project/edit")
-    public String editProject(@ModelAttribute ProjectsEntity projectToBeEdited){
+    public String editProject(@ModelAttribute ProjectsEntity projectToBeEdited) {
         projectService.editProject(projectToBeEdited);
         return "redirect:/getProjects";
     }
 
     @GetMapping(path = "delete-project/{id}")
-    public String deleteProject(Model model, @PathVariable("id") Integer id){
+    public String deleteProject(Model model, @PathVariable("id") Integer id) {
         projectService.deleteProject(id);
         return "redirect:/getProjects";
     }
--- projectmanagement/src/main/java/com/sda/projectmanagement/controllers/UserController.java
+++ projectmanagement/src/main/java/com/sda/projectmanagement/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.sda.projectmanagement.controllers;
 
-import com.sda.projectmanagement.persistence.ProjectsEntity;
 import com.sda.projectmanagement.persistence.UsersEntity;
 import com.sda.projectmanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -24,9 +24,6 @@ public class UserController {
         return "users";
     }
 
-
-
-
     @GetMapping(path = "add-user")
     public String addUserPage(Model model) {
         model.addAttribute("newUser", new UsersEntity());
@@ -37,6 +34,24 @@ public class UserController {
     public String addUser(@ModelAttribute UsersEntity newUser) {
         userService.addUser(newUser);
         return "redirect:/getUsers";
+    }
+    @GetMapping(path = "edit-user/{id}")
+    public String editUserPage(Model model, @PathVariable("id") Integer id){
+        UsersEntity usersEntity = userService.getUser(id);
+        model.addAttribute("userToBeEdited", usersEntity);
+        return "edit-user";
+    }
+
+    @PostMapping(path = "user/edit")
+    public String editUser(@ModelAttribute UsersEntity userToBeEdited){
+        userService.editUser(userToBeEdited);
+        return "redirect:/getUsers";
+    }
+
+    @GetMapping(path = "delete-user/{id}")
+    public String deleteUser(Model model, @PathVariable("id") Integer id){
+        userService.deleteUser(id);
+        return "redirect:/getUser";
     }
 
 }
